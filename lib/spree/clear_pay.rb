b67634f4078d7e2c0a718aa19771ac70906ca38f
Bryan Mahoney <bryan@goDynamo.com>
2010-05-03 13:56:19 -0400

    process payment when notification script is fired

--- lib/spree/clear_pay.rb
+++ lib/spree/clear_pay.rb
@@ -12,40 +12,6 @@ module Spree::ClearPay
   def payment_success
     load_object
     @gateway = payment_method
-    
-    # record a payment
-      
-    fake_card = Creditcard.new :cc_type        => "visa",
-                               :month          => Time.now.month, 
-                               :year           => Time.now.year, 
-                               :first_name     => @order.bill_address.firstname, 
-                               :last_name      => @order.bill_address.lastname,
-                               :verification_value => 'clearpay',
-                               :number => "clearpay"
-                               
-    payment = @order.checkout.payments.create(:amount => @order.total, 
-                                           :source => fake_card,
-                                           :payment_method_id => params[:payment_method_id])
-
-    # query - need 0 in amount for an auth? see main code
-    transaction = CreditcardTxn.new( :amount => @order.total,
-                                     :response_code => 'success',
-                                     # :payment_status => 'paid',
-                                     :txn_type => CreditcardTxn::TxnType::PURCHASE)
-    
-
-    payment.txns << transaction  
-
-
-    
-    @order.save!
-    @checkout.reload
-    #need to force checkout to complete state
-    until @checkout.state == "complete"
-      @checkout.next!
-    end
-    complete_checkout
-    payment.finalize!
 
   end
 
